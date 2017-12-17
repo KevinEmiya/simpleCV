804c8807f70697db884c4339340bd44bcf7b0748
kevinyang <kevin_yang1988@163.com>
2017-12-17 22:22:19 +0800

    Add supported cv::Mat types for convertion into QImage

--- demos/QCvUtils/src/util/QCvDataUtils.cpp
+++ demos/QCvUtils/src/util/QCvDataUtils.cpp
@@ -1,11 +1,44 @@
 #include "QCvDataUtils.h"
 
+#include <QDebug>
 #include <QImage>
 
 QImage QCvDataUtils::cvMatToQImage(const cv::Mat& mat)
 {
-    return QImage((const unsigned char*) (mat.data),
-                           mat.cols, mat.rows,
-                           QImage::Format_RGB888)
-                        .rgbSwapped();
+    const unsigned char* data = mat.data;
+    int width = mat.cols;
+    int height = mat.rows;
+    int bytesPerLine = static_cast<int>(mat.step);
+    switch (mat.type())
+    {
+        // 8bit, ARGB
+        case CV_8UC4:
+        {
+            QImage image(data, width, height, bytesPerLine,
+                         QImage::Format_ARGB32);
+            return image;
+        }
+        // 8bit, BGR
+        case CV_8UC3:
+        {
+            QImage image(data, width, height, bytesPerLine,
+                         QImage::Format_RGB888);
+            //swap blue and red channel
+            return image.rgbSwapped();
+        }
+        // 8bit, Grayshale
+        case CV_8UC1:
+        {
+            QImage image(data, width, height, bytesPerLine,
+                         QImage::Format_Grayscale8);
+            return image;
+        }
+        default:
+        {
+            // Unsupported format
+            qWarning() << "Unsupported cv::Mat tpye: " << mat.type()
+                       << ", Empty QImage will be returned!";
+            return QImage();
+        }
+    }
 }
